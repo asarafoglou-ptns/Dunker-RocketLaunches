866b85a2281f474a06347cd0d2d7674ec8d05a64
Laureen Dunker <laureen.dunker@web.de>
2024-05-27 10:18:12 +0200

    Week2 Pull request with new function
    Includes new function get_GUI to call the shiny app and all updated descriptions.

--- RocketsNEW/R/Functions_Laureen.R
+++ RocketsNEW/R/Functions_Laureen.R
@@ -1,5 +1,3 @@
-#Programming week 1 pull request functions
-
 #' @title getRocketData
 #' @description Get the current data from Launch Library 2 API, which is needed to run the Rocket Launches Shiny App.
 #' @param none no parameters needed
@@ -38,3 +36,112 @@ getRocketData <- function(){
   return(data_rockets)
 }
 
+
+#' @title get_GUI
+#' @description Function to Call the Shiny App "Rocket Launches 2024".
+#' @param none no parameters needed
+#' @return Returns a GUI of a Shiny App that displays rocket launches on a world map. These can be filtered according to success, provider & time frame.
+#' @examples
+#' # get_GUI()
+#' @note If Error occurs: "Error in curl::curl_fetch_memory(url, handle = handle) : etc", run get_GUI() again until the data is returned.
+#' @export
+get_GUI <- function(){
+  
+  tempdir(check = TRUE)
+  library(dplyr)
+  library(bslib)
+  
+  data_rockets <- getRocketData()
+  
+  ##shiny app 
+  ui <- shiny::fluidPage(
+    
+    theme = bslib::bs_theme(
+      bootswatch = "flatly",
+      primary = "#2C3E50"
+    ),
+    
+    tags$style(HTML("
+    #map {
+      height: calc(100vh - 80px) !important;
+    }
+    .leaflet-container {
+      height: 100% !important;
+      width: 100% !important;
+    }
+  ")),
+    
+    shiny::titlePanel(title = tags$h1(
+      tags$b("Rocket Launches 2024"),
+      tags$style(HTML("h1 { text-align: center; }")))),
+    
+    shiny::sidebarLayout(
+      
+      sidebarPanel = shiny::sidebarPanel(
+        shiny::helpText("You can filter the shown rocket launches according to the following options:"),
+        shiny::br(),
+        shiny::br(),
+        shiny::sliderInput("month", "Months of 2024", min = 1, max = 12, value = c(1,5)),
+        shiny::checkboxGroupInput("status", 
+                                  label = "Launch Status", 
+                                  choices = list("Success" = 3, 
+                                                 "Failure" = 4),
+                                  selected = c(3, 4)),
+        shiny::checkboxGroupInput("category", 
+                                  label = "Provider Category", 
+                                  choices = unique(data_rockets$provider_category),
+                                  selected = unique(data_rockets$provider_category)
+        ),
+        shiny::helpText("Click on the dots to get more information on the launched objects."),
+        
+        width = 3
+      ),
+      
+      mainPanel = shiny::mainPanel(
+        leaflet::leafletOutput(outputId = "map"),
+        width = 9
+      )
+    )
+  )
+  
+  
+  
+  server <- function(input, output){
+    
+    map_df <- shiny::reactive({
+      
+      data_rockets %>%
+        dplyr::filter(month >= input$month[1] & month <= input$month[2]) %>%
+        dplyr::filter(success_id %in% input$status) %>%
+        dplyr::filter(provider_category %in% input$category)
+      
+    })
+    
+    output$map <- leaflet::renderLeaflet({
+      
+      leaflet::leaflet() %>%
+        leaflet::addTiles() %>%
+        leaflet::setView(lng = 15, lat = 10, zoom = 1.5) %>%
+        leaflet::addCircleMarkers(data = map_df(), 
+                                  ~longitude, 
+                                  ~latitude, 
+                                  radius = 3,
+                                  color = ~ifelse(success_id == 3, "green", "red"),
+                                  popup = ~paste(
+                                    "<strong>Name:</strong>", name, "<br>",
+                                    "<strong>Status:</strong>", success_abbrev, "<br>",
+                                    "<strong>Time of Launch:</strong>", time_of_launch,"<br>",
+                                    "<strong>Provider Type:</strong>", provider_category, "<br>",
+                                    "<strong>Provider Name:</strong>", provider_name, "<br>",
+                                    "<strong>Purpose:</strong>", mission_purpose,"<br>",
+                                    "<strong>Description:</strong>", mission_description,"<br>",
+                                    "<img src =", image_URL, "width = '100px'>"
+                                  ))
+      
+    })
+    
+  }
+  
+  shiny::shinyApp(ui, server)
+  
+}
